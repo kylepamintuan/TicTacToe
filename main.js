54e22a9fe3d145757341362f81cf92217c0f3cdf
nebevets <steveben@exmsft.com>
2018-08-22 10:26:09 -0700

    makeGameBoard function created

---
+++ main.js
@@ -0,0 +1,18 @@
+function makeGameBoard( size ){
+    var squareSize = 100 / size;
+    for (var rowIndex=0; rowIndex < size; rowIndex++){
+        for (var colIndex=0; colIndex < size; colIndex++){
+            var square = $('<div>', {
+            'class': 'square',
+            row: rowIndex,  // custom attribute for row number
+            col: colIndex,  // custom attribute for col number
+            width: squareSize + 'vmin',
+            height: squareSize + 'vmin'
+            });
+            if (colIndex === 0){
+                square.css('clear', 'left');
+            }
+            $('.gameBoard').append( square );
+        }
+    }
+}
