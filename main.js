f50b416195fa00ee00dc4d85954f4b9f2b2bb14f
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-08-22 10:33:42 -0700

    added some empty js functions to start the gameplay.

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(initializeApp);
+
+function initializeApp()
+{
+	console.log('Initializing App...');
+}
+
+function bindEventHandlers()
+{
+
+}
+
+function fillGameBoard()
+{
+	var game_board = $('.game_board');
+}
